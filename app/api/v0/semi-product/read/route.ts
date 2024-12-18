519984cc385289929fdadcde48e044005d092ab7
qkrtjddms587 <109716738+qkrtjddms587@users.noreply.github.com>
2024-12-18 16:52:18 +0900

    error

--- app/api/v0/semi-product/read/route.ts
+++ app/api/v0/semi-product/read/route.ts
@@ -1,10 +1,14 @@
 import db from "@/app/lib/db";
-import { NextRequest, NextResponse } from "next/server";
+import { NextResponse } from "next/server";
 
 export async function GET() {
-  const products = await db.semiProduct.findMany({
+  const semiProducts = await db.semiProduct.findMany({
     where: { deletedAt: null },
   });
-  if (!products) return NextResponse.json({ success: false }, { status: 400 });
-  return NextResponse.json({ success: true, data: products }, { status: 200 });
+  if (!semiProducts)
+    return NextResponse.json({ success: false }, { status: 400 });
+  return NextResponse.json(
+    { success: true, data: semiProducts },
+    { status: 200 }
+  );
 }
